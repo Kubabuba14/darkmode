aa3de382c24bf32e22493800f20832dc3175ac30
Kubabuba14 <jakubwit@hotmail.co.uk>
2023-10-20 17:38:14 +0200

    dry

--- script.js
+++ script.js
@@ -12,34 +12,38 @@ function imageMode(color) {
     image2.src = `img/undraw_feeling_proud_${color}.svg`;
     image3.src = `img/undraw_conceptual_idea_${color}.svg`;
 }
-// Dark Mode Styles
-function darkMode () {
-    nav.style.backgroundColor = 'rgb (0 0 0 /50%)'
-    textBox.style.backgroundColor = 'rgb(255 255 255 / 50%)';
-    toggleIcon.children[0].textContent = 'Dark Mode';
-    toggleIcon.children[1].classList.replace('fa-sun', 'fa-moon');
-    imageMode('dark')
-}
 
-// Light Mode Styles
-function lightMode () {
-    nav.style.backgroundColor = 'rgb (225 225 225 /50%)'
-    textBox.style.backgroundColor = 'rgb(0 0 0 / 50%)';
-    toggleIcon.children[0].textContent = 'Light Mode';
-    toggleIcon.children[1].classList.replace('fa-moon', 'fa-sun');
-    imageMode('light');
+function toggleDarkLightMode (isLight) {
+    nav.style.backgroundColor = isLight ? 'rgb (225 225 225 /50%)' : 'rgb (0 0 0 /50%)';
+    textBox.style.backgroundColor = isLight ?'rgb(0 0 0 / 50%)' : 'rgb(255 255 255 / 50%)';
+    toggleIcon.children[0].textContent = isLight ? 'Light Mode' : 'Dark Mode';
+    isLight ? toggleIcon.children[1].classList.replace('fa-moon', 'fa-sun') : toggleIcon.children[1].classList.replace('fa-sun', 'fa-moon'); 
+    isLight ? imageMode('light') : imageMode('dark');
 }
 
 // Switch Theme Dynamicically
 function switchTheme(event) {
     if (event.target.checked) {
         document.documentElement.setAttribute('data-theme', 'dark');
-        darkMode();
+        localStorage.setItem('theme', 'dark')
+        toggleDarkLightMode(false);
     } else {
         document.documentElement.setAttribute('data-theme', 'light');
-        lightMode();
+        localStorage.setItem('theme', 'light')
+        toggleDarkLightMode(true);
     }
 }
 
 // Event Listener
 toggleSwitch.addEventListener('change', switchTheme);
+
+// Check Local Storage for theme
+const currentTheme = localStorage.getItem('theme');
+if (currentTheme) {
+    document.documentElement.setAttribute('data-theme', currentTheme);
+
+    if (currentTheme === 'dark') {
+        toggleSwitch.checked = true;
+        toggleDarkLightMode(false);
+    }
+}
